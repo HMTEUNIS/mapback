c36a035dff3f80da3d44204d308fd3507bc44bbb
Holly Teunis <+>
2021-12-09 23:03:57 -0500

    form complete. needs work on the post requests for user and suggestion, but the objects are generated. hopefully ill get it done before i go to sleep

--- phase-3-sinatra-react-project/sanfran-map/src/AddASuggestion.js
+++ phase-3-sinatra-react-project/sanfran-map/src/AddASuggestion.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 function AddASuggestion ({selected, selectedid}){
     const [hoods, sethoods] = useState ([])
+    const [users, setUsers] = useState ([])
+    const [localyn, setlocalyn] = useState (false)
 
     useEffect(() => {
         fetch(`http://localhost:9292/neighborhood`)
@@ -9,7 +11,13 @@ function AddASuggestion ({selected, selectedid}){
              sethoods(data)
         })
       }, []);
-      console.log("hoods", hoods)
+      useEffect(() => {
+        fetch(`http://localhost:9292/user`)
+        .then ((res) => res.json())
+        .then ((data ) => {
+             setUsers(data)
+        })
+      }, []);
 
 const [E, setE] = useState("")
 const [L, setL] = useState("")
@@ -22,6 +30,16 @@ const [UID, setUID] = useState(2)
 const [A, setA] = useState('')
 const [name, setName] =useState('')
 
+function createUser (e) {
+    e.preventDefault()
+
+const newUser = {
+    name: name,
+    local: localyn
+    
+}
+console.log(newUser)
+}
 function handleSubmit (e) {
     e.preventDefault()
     const newSugg = {
@@ -34,62 +52,71 @@ function handleSubmit (e) {
         neighborhood_id: parseInt(NID),
         user_id: parseInt(UID)
     }
-console.log("sel", selectedid)
-console.log('POST', newSugg)}
 
-    // fetch('http://localhost:9292/suggestion', {
-    // method: 'POST', 
-    // headers: {
-    // 'Content-Type': 'application/json',
-    // },
-    // body: JSON.stringify(newSugg),
-    // })
-    // .then(res => res.json())
-    // .then(data => {
-    // console.log("test");
+console.log('POST', newSugg)
 
-    // })
-    // .catch((error) => {
-    // console.error('Error:', error);
-    // });
-    // setE("")
-    // setL('')
-    // setD('')
-    // setW('')
-    // setR("")
-    // setC("")
-    // }
+    fetch('http://localhost:9292/suggestion', {
+    method: 'POST', 
+    headers: {
+    'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newSugg),
+    })
+    .then(res => res.json())
+    .then(data => {
+    console.log("test");
+
+    })
+    .catch((error) => {
+    console.error('Error:', error);
+    });
+   
+    }
 
     return (
     
-    /* <div className="new user">
-    <input type="text" name="name" placeholder="name?" onChange={e => setName(e.target.value)} />
-    <input type="number" name="age" placeholder="age?" step="1" onChange={e => setA(e.target.value)} />
-<input type="text" name="local" placeholder="local?" />
-
-  </div>
-  <button className="ui button" type="submit"> </ button> */
+    
   <div>
-  <form onSubmit={handleSubmit}>
+  <form onSubmit={handleSubmit, createUser}>
     <div className="comment form">
-    <input type="text" name="eat" placeholder="eat!" onChange={e => setE(e.target.value)} />
-    <input type="text" name="learn" placeholder="learn!" onChange={e => setL(e.target.value)}/>
-    <input type="text" name="watch" placeholder="watch!" onChange={e => setW(e.target.value)}/>
-    <input type="text" name="do" placeholder="do!" onChange={e => setD(e.target.value)} />
-    <select name="rating" id="rating" onChange={e => setR(e.target.value)}> 
+    <select name="user" id="user" onChange={e => setUID(e.target.value)}>
+        { users.map (user =>
+   <option value= {user.id} key= {user.id} >{user.name}</option >)
+      }
+    <option value={users.length + 1} >New User</option >
+</select>
+<input type="text" name="name" placeholder="New User? what's your name?" onChange={e => setName(e.target.value)}/>
+<select name="local" id="local" onChange={e => setlocalyn(e.target.value)}> 
+   <option value="true" >Local</option >
+ <option value="false" >Visitor</option >
+ 
+</select>
+
+<br />
+
+
+    <select name="neighborhood" id="neighborhood" onChange={e => setNID(e.target.value)}>
+        { hoods.map (hood =>
+   <option value= {hood.id} key= {hood.id} >{hood.name}</option >)
+      }
+</select>
+Rating:
+<select name="rating" id="rating" onChange={e => setR(e.target.value)}> 
    <option value="1" >1</option >
  <option value="2" >2</option >
  <option value="3" >3</option >
  <option value="4" >4</option >
- <option value="5" >5</option >)
+ <option value="5" >5</option >
       
 </select>
+    <input type="text" name="eat" placeholder="eat!" onChange={e => setE(e.target.value)} />
+    <input type="text" name="learn" placeholder="learn!" onChange={e => setL(e.target.value)}/>
+    <input type="text" name="watch" placeholder="watch!" onChange={e => setW(e.target.value)}/>
+    <input type="text" name="do" placeholder="do!" onChange={e => setD(e.target.value)} />
+    <br />
+    <input type="text" name="comment" placeholder="comment?" onChange={e => setC(e.target.value)}/>
 
-    <select name="neighborhood" id="neighborhood" onChange={e => setNID(e.target.value)}>
-        { hoods.map (hood =>
-   <option value= {hood.id} >{hood.name}</option >)
-      }
-</select>
+    
 
   </div>
   <button className="ui button" type="submit">Add Tip! </ button>
